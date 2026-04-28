e7fb4ac28119e5460966686c37ec959aac09c32c
Sinterchlas <ichlasrifqi1@gmail.com>
2026-04-29 00:30:59 +0700

    feat: add peak detection algorithm and fix kalman bias
    the algorithm uses static thresholding obtain from several data observation. the detection is still buggy and not responsive, currently the peak is correctly detected but skips one gait cycle for each leg. fix kalman bias because the plus and minus sign opposites. the fix was done by using minus in the sensorcalc() and reverting it back in the peakdetection()

---
+++ GaitPhaseDetect_KalmanFilter.ino
@@ -0,0 +1,52 @@
+#include <arduino.h>
+#include <math.h>
+#include "functions.h"
+#include "config.h"
+unsigned long previousTime;
+
+void setup() {
+  Serial.begin(115200);
+  analogReadResolution(12);
+  Gy_caliberation();
+  previousTime = micros(); //mungkin bisa juga pake millis()
+}
+
+void loop() {
+  unsigned long currentTime = micros();
+  dt = (currentTime - previousTime) / 1000000.0;
+  previousTime = currentTime;
+
+  readsensors();
+  //printraw();
+  sensorRate();
+  sensorCalc();
+  kalmanfilter();
+  //peakdetection();
+
+  /*// bad output
+  Serial.print(GyL_angle); // heavily drifted integrated angval
+  Serial.print("\t");  
+  Serial.print(Tilt_angleL); // heavily noisy tilt from accelero
+  Serial.print("\t");      
+  Serial.print(GyR_angle); // heavily drifted integrated angval
+  Serial.print("\t");  
+  Serial.println(Tilt_angleR); // heavily noisy tilt from accelero
+  */
+
+  
+  // good output
+  //Serial.print(GyL_rate); // Gyro with bias
+  Serial.print(GyL_stable);
+  Serial.print("\t");        
+  Serial.print(-TiltKalL);
+  Serial.print("\t");  
+  //Serial.print(GyR_rate);// Gyro with bias
+  Serial.print(GyR_stable);
+  Serial.print("\t");         
+  Serial.print(-TiltKalR); 
+  Serial.print("\t");
+  Serial.println(gaitState);
+  delay(10);
+}
+
+
